d5a6c0d33c9f460588939114826e32a3b85bb9b0
Romulo Rocha <romulorocha063@gmail.com>
2019-10-17 23:32:21 -0300

    add auth module

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,60 @@
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { Types, signInSuccess, signFailure } from './actions';
+import api from '~/services/api';
+import history from '~/services/history';
+
+export function* singIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, '/sessions', { email, password });
+
+    const { token, user } = response.data;
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
+  }
+}
+
+export function* singUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, '/users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest(Types.SIGN_IN_REQUEST, singIn),
+  takeLatest(Types.SIGN_UP_REQUEST, singUp),
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest(Types.SIGN_OUT, signOut),
+]);
