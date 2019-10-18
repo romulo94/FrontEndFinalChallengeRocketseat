63388c95f0aff3759add3891e76edf440b180eff
Romulo Rocha <romulorocha063@gmail.com>
2019-10-17 23:01:43 -0300

    setting persist reducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import './config/ReactotronConfig';
-import store from './store';
+import { store, persistor } from './store';
 
 export default function App() {
   return (
     <>
       <Provider store={store}>
-        <Routes />
-        <GlobalStyle />
+        <PersistGate persistor={persistor}>
+          <Routes />
+          <GlobalStyle />
+        </PersistGate>
       </Provider>
     </>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-/* eslint-disable no-console */
-import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+import persistReducers from './persistReducers';
+
+import createStore from './createStore';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -9,18 +11,15 @@ const sagaMonitor =
   process.env.NODE_ENV === 'development'
     ? console.tron.createSagaMonitor()
     : null;
+
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
-const enhancer =
-  process.env.NODE_ENV === 'development'
-    ? compose(
-        console.tron.createEnhancer(),
-        applyMiddleware(sagaMiddleware)
-      )
-    : applyMiddleware(sagaMiddleware);
+const middleware = [sagaMiddleware];
 
-const store = createStore(rootReducer, enhancer);
+const store = createStore(persistReducers(rootReducer), middleware);
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
