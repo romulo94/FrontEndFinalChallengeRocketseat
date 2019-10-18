195ae0e1fd2e99130bac9059ec6662c52505793b
Romulo Rocha <romulorocha063@gmail.com>
2019-10-17 23:11:09 -0300

    add history

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import Routes from './routes';
-import GlobalStyle from './styles/global';
+import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
+import history from './services/history';
+import Routes from './routes';
 import { store, persistor } from './store';
+import GlobalStyle from './styles/global';
 
 export default function App() {
   return (
     <>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <Routes />
-          <GlobalStyle />
+          <Router history={history}>
+            <Routes />
+            <GlobalStyle />
+          </Router>
         </PersistGate>
       </Provider>
     </>
